9e95359a9d0c2721bb47917d9ff490ee8b886e07
xuhaijun <Haijun.Xu2@lotuscars.com.cn>
2023-06-07 23:47:45 +0800

    添加vue-router

---
+++ src/router.ts
@@ -0,0 +1,12 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+// 路由列表
+const routes = [
+
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+export default router;
